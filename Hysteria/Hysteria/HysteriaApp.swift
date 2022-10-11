86d330e8f4456b816ca3daf4bd669de766c86496
강수진 <sujikang@gmarket.com>
2022-10-11 23:05:51 +0900

    Create initial project

---
+++ Hysteria/Hysteria/HysteriaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HysteriaApp.swift
+//  Hysteria
+//
+//  Created by 강수진 on 2022/10/11.
+//
+
+import SwiftUI
+
+@main
+struct HysteriaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
